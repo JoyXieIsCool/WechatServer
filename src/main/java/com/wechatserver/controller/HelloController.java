7232c4cf02bc7c91b56428e16b825a27a193f8aa
Joey <joy.junrong.xie@gmail.com>
2016-04-04 17:45:31 +0800

    add favicon.ico

--- src/main/java/com/wechatserver/controller/HelloController.java
+++ src/main/java/com/wechatserver/controller/HelloController.java
@@ -5,12 +5,14 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Date;
+
 @Controller
-@RequestMapping("/")
+@RequestMapping("/test.do")
 public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
+		model.addAttribute("message", "Current Time: " + new Date());
 		return "hello";
 	}
 }
--- src/main/webapp/WEB-INF/pages/hello.jsp
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -1,4 +1,8 @@
 <html>
+<head>
+	<title>I'm Joey</title>
+	<!--<link rel="shortcut icon" type="image/x-icon" href="http://localhost:8080/favicon.ico">-->
+</head>
 <body>
 	<h1>${message}</h1>
 </body>
