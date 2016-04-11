374033db1cf6716e59086c3ab1c7a54b4fa79140
JoeyXieIsCool <joy.junrong.xie@gmail.com>
2016-04-11 18:56:41 +0800

    添加SLF4J和Log4J日志模块

--- src/main/java/com/wechatserver/controller/HelloController.java
+++ src/main/java/com/wechatserver/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.wechatserver.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,12 @@ import java.util.Date;
 @Controller
 @RequestMapping("/test")
 public class HelloController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model, String signature, String timestamp, String nonce, String echostr) {
+		logger.debug("~~~~~~~~ yo yo yo ~~~~~~~~~~");
 		System.out.println("in~~" + timestamp);
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日 hh:mm:ss");
 		String result = formatter.format(new Date());
